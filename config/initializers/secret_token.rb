719c1b94b8744f07e61ceea23f8e6a7ba5da44ef
czhu12 <chris_zhu12@hotmail.com>
2013-03-28 23:15:30 -0700

    initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomfeed::Application.config.secret_token = '4708ea333ed818ebc6751c98781cf1ad3dc4d11293a447ea2be136c34bdcdf246d4bd8604db8d20aec869ed4f950638d923fff69896f6877004e1892f5300c66'
