a0822d614b640f17904ce3df7d26fb6f47a0b4bd
czhu12 <chris_zhu12@hotmail.com>
2013-04-01 00:21:44 -0700

    added answer and room association

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,6 @@ class Answer < ActiveRecord::Base
  
   belongs_to :user
   belongs_to :question
+
+  has_one :room, :through=>:question
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,11 @@
 class Room < ActiveRecord::Base
   attr_accessible :name, :maximum_registrants, :owner_id
+
+  #users association
+  has_many :registrations
+  has_many :users, :through=>:registrations
+
+  #questions association
+  has_many :questions
 end
+
