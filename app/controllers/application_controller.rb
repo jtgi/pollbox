55cac64b1eed2133a43b22b410731080fa1491db
Chris <chris.zhu12@gmail.com>
2013-06-18 17:14:42 -0700

    added mobile folders to assets/javascripts and assets/stylesheets, changed application controller/ backbone_root.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
+
+  private
+  def mobile_device?
+    request.user_agent =~ /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/
+
+  end
+  helper_method :mobile_device?
 end
